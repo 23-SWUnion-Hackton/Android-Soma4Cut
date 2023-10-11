bb2c244024097441e3c09c4b3c255a79cb343035
stev3j <mygajeong@gmail.com>
2023-10-11 20:42:25 +0900

    💄 :: Design LoginFragment

--- presentation/src/main/java/com/staker4wapper/android_soma4cut/feature/start/LoginFragment.kt
+++ presentation/src/main/java/com/staker4wapper/android_soma4cut/feature/start/LoginFragment.kt
@@ -8,9 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.sigma.flick.base.BaseFragment
 import com.staker4wapper.android_soma4cut.R
-import com.staker4wapper.android_soma4cut.databinding.FragmentStartBinding
+import com.staker4wapper.android_soma4cut.databinding.FragmentLoginBinding
 
-class StartFragment: BaseFragment<FragmentStartBinding, StartViewModel>(R.layout.fragment_start) {
+class LoginFragment: BaseFragment<FragmentLoginBinding, StartViewModel>(R.layout.fragment_login) {
 
     override val viewModel: StartViewModel by viewModels()
 
---
+++ presentation/src/main/java/com/staker4wapper/android_soma4cut/feature/start/StartViewModel.kt
@@ -0,0 +1,6 @@
+package com.staker4wapper.android_soma4cut.feature.start
+
+import com.staker4wapper.android_soma4cut.base.BaseViewModel
+
+class StartViewModel: BaseViewModel() {
+}
