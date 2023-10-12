858be19008c58f900299530615d6b7e1286580fd
stev3j <mygajeong@gmail.com>
2023-10-12 10:36:41 +0900

    💄 :: Design SettingFragment

---
+++ presentation/src/main/java/com/staker4wapper/android_soma4cut/feature/setting/SettingFragment.kt
@@ -0,0 +1,13 @@
+package com.staker4wapper.android_soma4cut.feature.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sigma.flick.base.BaseFragment
+import com.staker4wapper.android_soma4cut.R
+
+class SettingFragment: BaseFragment<>() {
+
+}
